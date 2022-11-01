6b2c9bf3d9519a62017a3c964a6d57b2a0009ce2
Thea Flowers <thea@winterbloom.com>
2022-10-31 22:04:48 -0400

    Add rendering for NoConnect, HierarchicalLabel, and fix text orientation and alignment.

--- render.js
+++ render.js
@@ -3,32 +3,51 @@ import { convert_arc_to_center_and_angles } from "./arc.js";
 import { TransformStack } from "./transform_stack.js";
 import { BBox } from "./bbox.js";
 
-
 class Style {
     constructor(e) {
         const css = window.getComputedStyle(e);
-        this.font_family = css.getPropertyValue("--font-family").trim() || "sans";
-        this.font_size = parseFloat(css.getPropertyValue("--font-size").trim() || 2.54);
-        this.line_spacing = parseFloat(css.getPropertyValue("--line-spacing").trim() || 1.5);
-        this.background = css.getPropertyValue("--background").trim() || "#131218";
+        this.font_family =
+            css.getPropertyValue("--font-family").trim() || "sans";
+        this.font_size = parseFloat(
+            css.getPropertyValue("--font-size").trim() || 2.54
+        );
+        this.line_spacing = parseFloat(
+            css.getPropertyValue("--line-spacing").trim() || 1.5
+        );
+        this.background =
+            css.getPropertyValue("--background").trim() || "#131218";
         this.stroke = css.getPropertyValue("--stroke").trim() || "#F8F8F0";
-        this.stroke_width = parseFloat(css.getPropertyValue("--stroke-width").trim() || 0.254);
+        this.stroke_width = parseFloat(
+            css.getPropertyValue("--stroke-width").trim() || 0.254
+        );
         this.fill = css.getPropertyValue("--fill").trim() || "#F8F8F0";
-        this.highlight = css.getPropertyValue("--highlight").trim() || "#C8FFE3";
-        this.symbol_fill = css.getPropertyValue("--symbol-fill").trim() || "#433E56";
-        this.symbol_stroke = css.getPropertyValue("--symbol-stroke").trim() || "#C5A3FF";
-        this.symbol_field = css.getPropertyValue("--symbol-field").trim() || "#AE81FF";
-        this.symbol_ref = css.getPropertyValue("--symbol-ref").trim() || "#81EEFF";
-        this.symbol_value = css.getPropertyValue("--symbol-value").trim() || "#81EEFF";
+        this.highlight =
+            css.getPropertyValue("--highlight").trim() || "#C8FFE3";
+        this.symbol_fill =
+            css.getPropertyValue("--symbol-fill").trim() || "#433E56";
+        this.symbol_stroke =
+            css.getPropertyValue("--symbol-stroke").trim() || "#C5A3FF";
+        this.symbol_field =
+            css.getPropertyValue("--symbol-field").trim() || "#AE81FF";
+        this.symbol_ref =
+            css.getPropertyValue("--symbol-ref").trim() || "#81EEFF";
+        this.symbol_value =
+            css.getPropertyValue("--symbol-value").trim() || "#81EEFF";
         this.wire = css.getPropertyValue("--wire").trim() || "#AE81FF";
         this.junction = css.getPropertyValue("--junction").trim() || "#DCC8FF";
-        this.no_connect = css.getPropertyValue("--no-connect").trim() || "#FF81AD";
-        this.pin_radius = parseFloat(css.getPropertyValue("--pin-radius").trim() || 0.254);
-        this.pin_color = css.getPropertyValue("--pin-color").trim() || "#81FFBE";
+        this.no_connect =
+            css.getPropertyValue("--no-connect").trim() || "#FF81AD";
+        this.pin_radius = parseFloat(
+            css.getPropertyValue("--pin-radius").trim() || 0.254
+        );
+        this.pin_color =
+            css.getPropertyValue("--pin-color").trim() || "#81FFBE";
         this.pin_name = css.getPropertyValue("--pin-name").trim() || "#81FFBE";
-        this.pin_number = css.getPropertyValue("--pin-number").trim() || "#64CB96";
+        this.pin_number =
+            css.getPropertyValue("--pin-number").trim() || "#64CB96";
         this.label = css.getPropertyValue("--label").trim() || "#DCC8FF";
-        this.hierarchical = css.getPropertyValue("--hierarchical-label").trim() || "#A3FFCF";
+        this.hierarchical_label =
+            css.getPropertyValue("--hierarchical-label").trim() || "#A3FFCF";
     }
 }
 
@@ -73,10 +92,11 @@ export class Renderer {
 
         const w_scale = canvas_rect.width / bb.w;
         const h_scale = canvas_rect.height / bb.h;
-        const scale = Math.min(w_scale, h_scale);
+        let scale = Math.min(w_scale, h_scale);
 
         // crop height
         if (crop) {
+            scale = w_scale;
             const new_height = scale * bb.h;
             this.cvs.height = Math.round(new_height * window.devicePixelRatio);
             this.cvs.style.height = `${new_height}px`;
@@ -129,8 +149,11 @@ export class Renderer {
 
     interactive_bboxes(v) {
         const out = [];
-        for(const g of v.iter_graphics()) {
+        for (const g of v.iter_graphics()) {
             if ([types.SymbolInstance].includes(g.constructor)) {
+                if (g.lib_symbol?.power) {
+                    continue;
+                }
                 out.push(this.bbox(g));
             }
         }
@@ -144,15 +167,32 @@ export class Renderer {
 
         const new_effects = window.structuredClone(effects);
         const transform = this.transforms.abs;
+        transform.normalize_rotation();
+
         let new_rotation = 0;
 
-        if (transform.rotation == 180) {
+        if (transform.rotation == 180 || transform.rotation == 270) {
             new_rotation = -180;
+            if (new_effects?.v_align == "center") {
+                if (new_effects?.h_align == "left") {
+                    new_effects.h_align = "right";
+                } else if (new_effects?.h_align == "right") {
+                    new_effects.h_align = "left";
+                }
+            }
+        }
+
+        if (transform.flip_x) {
             if (new_effects?.h_align == "left") {
                 new_effects.h_align = "right";
             } else if (new_effects?.h_align == "right") {
                 new_effects.h_align = "left";
             }
+        }
+
+        if (transform.flip_y && transform.flip_x && transform.rotation == 90) {
+            new_rotation = 180;
+            //console.log(text, effects, new_effects, transform);
         }
 
         this.push(
@@ -165,17 +205,32 @@ export class Renderer {
 
         this.apply_Effects(new_effects);
 
-        callback();
+        const metrics = this.ctx.measureText(text);
+
+        // properly vertically center align text
+        let offset = 0;
+        if ((new_effects?.v_align || "center") == "center") {
+            const height =
+                metrics.actualBoundingBoxAscent +
+                metrics.actualBoundingBoxDescent;
+            offset = -metrics.actualBoundingBoxDescent + height / 2;
+        }
+
+        this.push(0, offset);
+        callback(text, metrics);
+        this.pop();
 
         this.pop();
     }
 
     draw_text_normalized(text, effects = null) {
         this.text_normalized_impl(text, effects, () => {
+            // this.ctx.save();
             // this.ctx.beginPath();
             // this.ctx.fillStyle = "pink";
-            // this.ctx.arc(0, 0, 1, 0, 360);
+            // this.ctx.arc(0, 0, 0.2, 0, 360);
             // this.ctx.fill();
+            // this.ctx.restore();
             this.ctx.fillText(text, 0, 0);
         });
     }
@@ -183,13 +238,13 @@ export class Renderer {
     bbox_text_normalized(text, effects = null) {
         let bb = null;
 
-        this.text_normalized_impl(text, effects, () => {
-            const metrics = this.ctx.measureText(text);
+        this.text_normalized_impl(text, effects, (_, metrics) => {
             bb = BBox.from_points(
                 -metrics.actualBoundingBoxLeft,
                 -metrics.actualBoundingBoxAscent,
-                metrics.actualBoundingBoxRight,
-                metrics.actualBoundingBoxDescent,
+                metrics.actualBoundingBoxRight + metrics.actualBoundingBoxLeft,
+                metrics.actualBoundingBoxDescent +
+                    metrics.actualBoundingBoxAscent,
                 this.transforms.mat,
                 text
             );
@@ -220,7 +275,7 @@ export class Renderer {
     }
 
     draw_KicadSch(sch) {
-        for(const g of sch.iter_graphics()) {
+        for (const g of sch.iter_graphics()) {
             this.ctx.fillStyle = this.style.fill;
             this.ctx.strokeStyle = this.style.stroke;
             this.draw(g);
@@ -229,7 +284,7 @@ export class Renderer {
 
     bbox_KicadSch(sch) {
         let bb = new BBox();
-        for(const g of sch.iter_graphics()) {
+        for (const g of sch.iter_graphics()) {
             const gbb = this.bbox(g);
             bb = BBox.combine(bb, gbb);
         }
@@ -380,15 +435,46 @@ export class Renderer {
         );
     }
 
+    draw_NoConnect(nc) {
+        const r = 0.7;
+        this.push(nc.at.x, nc.at.y);
+        this.ctx.strokeStyle = this.style.no_connect;
+        this.ctx.beginPath();
+        this.ctx.moveTo(-r, -r);
+        this.ctx.lineTo(r, r);
+        this.ctx.moveTo(r, -r);
+        this.ctx.lineTo(-r, r);
+        this.ctx.stroke();
+        this.pop();
+    }
+
+    bbox_NoConnect(j) {
+        const r = 1;
+        return BBox.from_points(
+            j.at.x - r,
+            j.at.y - r,
+            j.at.x + r,
+            j.at.y + r,
+            this.transforms.mat,
+            j
+        );
+    }
+
     apply_Effects(e) {
         if (!e) {
             this.ctx.font = `${this.style.font_size}px "${this.style.font_family}"`;
             this.ctx.textAlign = "center";
+            this.ctx.textBaseline = "center";
             return;
         }
 
         this.ctx.font = `${e.size.y * 1.5}px "${this.style.font_family}"`;
         this.ctx.textAlign = e.h_align;
+        this.ctx.textBaseline = {
+            top: "top",
+            center: "center",
+            bottom: "bottom",
+        }[e.v_align];
     }
 
     draw_Label(l) {
@@ -417,6 +503,55 @@ export class Renderer {
         return bb;
     }
 
+    draw_HierarchicalLabel(l) {
+        if (l.effects?.hide) {
+            return;
+        }
+
+        this.push(l.at.x, l.at.y, l.at.rotation);
+        this.ctx.fillStyle = this.style.hierarchical_label;
+        this.ctx.strokeStyle = this.style.hierarchical_label;
+
+        let s = 1.5;
+
+        this.ctx.beginPath();
+        if (l.shape == "input") {
+            this.ctx.moveTo(0, s / 2);
+            this.ctx.lineTo(s / 2, s / 2);
+            this.ctx.lineTo(s, 0);
+            this.ctx.lineTo(s / 2, -s / 2);
+            this.ctx.lineTo(0, -s / 2);
+        } else if (l.shape == "output") {
+            this.ctx.moveTo(s, s / 2);
+            this.ctx.lineTo(s / 2, s / 2);
+            this.ctx.lineTo(0, 0);
+            this.ctx.lineTo(s / 2, -s / 2);
+            this.ctx.lineTo(s, -s / 2);
+        } else if (l.shape == "bidirectional" || l.shape == "tri_state") {
+            this.ctx.moveTo(s / 2, s / 2);
+            this.ctx.lineTo(s, 0);
+            this.ctx.lineTo(s / 2, -s / 2);
+            this.ctx.lineTo(0, 0);
+        } else if (l.shape == "passive") {
+            this.ctx.moveTo(0, s / 2);
+            this.ctx.lineTo(s, s / 2);
+            this.ctx.lineTo(s, -s / 2);
+            this.ctx.lineTo(0, -s / 2);
+        }
+        this.ctx.closePath();
+        this.ctx.stroke();
+
+        this.push(s + 0.254, 0);
+        this.ctx.textBaseline = "middle";
+        this.draw_text_normalized(l.name, l.effects);
+        this.pop();
+        this.pop();
+    }
+
+    bbox_HierarchicalLabel(l) {
+        return this.bbox_Label(l);
+    }
+
     draw_Text(t) {
         if (t.effects?.hide) {
             return;
@@ -427,11 +562,12 @@ export class Renderer {
         const lines = t.text.split("\n");
         let y = t.at.y;
         for (const line of lines.reverse()) {
-            this.text_normalized_impl(line, t.effects, () => {
-                const metrics = this.ctx.measureText(line);
-                this.ctx.fillText(line, t.at.x, y);
+            this.push(t.at.x, y, t.at.rotation);
+            this.text_normalized_impl(line, t.effects, (text, metrics) => {
+                this.ctx.fillText(line, 0, 0);
                 y -= metrics.actualBoundingBoxAscent * this.style.line_spacing;
             });
+            this.pop();
         }
     }
 
@@ -450,14 +586,17 @@ export class Renderer {
             this.text_normalized_impl(line, t.effects, () => {
                 const metrics = this.ctx.measureText(line);
 
-                bb = BBox.combine(bb, BBox.from_points(
-                    -metrics.actualBoundingBoxLeft,
-                    y - metrics.actualBoundingBoxAscent,
-                    metrics.actualBoundingBoxRight,
-                    y + metrics.actualBoundingBoxDescent,
-                    this.transforms.mat,
-                    t
-                ));
+                bb = BBox.combine(
+                    bb,
+                    BBox.from_points(
+                        -metrics.actualBoundingBoxLeft,
+                        y - metrics.actualBoundingBoxAscent,
+                        metrics.actualBoundingBoxRight,
+                        y + metrics.actualBoundingBoxDescent,
+                        this.transforms.mat,
+                        t
+                    )
+                );
 
                 y -= metrics.actualBoundingBoxAscent * this.style.line_spacing;
             });
@@ -490,18 +629,18 @@ export class Renderer {
         /* Pin names & numbers */
         this.push(p.at.x, p.at.y, p.at.rotation);
 
-        this.ctx.textBaseline = "middle";
         this.ctx.fillStyle = this.style.pin_name;
         p.name_effects.h_align = "left";
+        p.number_effects.v_align = "center";
         if (!hide_name && p.name !== "~") {
             this.push(p.length + pin_name_offset);
             this.draw_text_normalized(p.name, p.name_effects);
             this.pop();
         }
 
-        this.ctx.textBaseline = "bottom";
         this.ctx.fillStyle = this.style.pin_number;
         p.number_effects.h_align = "center";
+        p.number_effects.v_align = "bottom";
         if (!hide_number) {
             this.push(p.length / 2);
             this.draw_text_normalized(p.number, p.number_effects);
@@ -523,11 +662,11 @@ export class Renderer {
     }
 
     draw_LibrarySymbol(s) {
-        for (const c of Object.values(s.children)) {
+        for (const c of Object.values(s.children).slice().reverse()) {
             this.draw_LibrarySymbol(c);
         }
 
-        for (const g of s.graphics) {
+        for (const g of s.graphics.slice().reverse()) {
             if (g.fill == "outline") {
                 this.ctx.fillStyle = this.style.symbol_stroke;
             } else {
@@ -558,12 +697,16 @@ export class Renderer {
                 p,
                 s.pin_name_offset,
                 s.hide_pin_names || hide_pin_names,
-                s.hide_pin_numbers || hide_pin_numbers,
+                s.hide_pin_numbers || hide_pin_numbers
             );
         }
 
         for (const c of Object.values(s.children)) {
-            this.draw_LibrarySymbol_Pins(c, s.hide_pin_names, s.hide_pin_numbers);
+            this.draw_LibrarySymbol_Pins(
+                c,
+                s.hide_pin_names,
+                s.hide_pin_numbers
+            );
         }
     }
 
@@ -601,7 +744,7 @@ export class Renderer {
         this.pop();
 
         for (const p of Object.values(si.properties)) {
-            this.draw_Property(si, p);
+            this.draw_Property(si, p, si.mirror);
         }
     }
 
@@ -622,12 +765,18 @@ export class Renderer {
         return bb;
     }
 
-    draw_Property(si, p) {
+    draw_Property(si, p, mirror) {
         if (p.hide || p.effects?.hide) {
             return;
         }
 
-        this.push(p.at.x, p.at.y, si.at.rotation + p.at.rotation);
+        this.push(
+            p.at.x,
+            p.at.y,
+            si.at.rotation + p.at.rotation,
+            mirror == "y"
+        );
+
         if (p.key == "Reference") {
             this.ctx.fillStyle = this.style.symbol_ref;
         } else if (p.key == "Value") {
@@ -635,7 +784,9 @@ export class Renderer {
         } else {
             this.ctx.fillStyle = this.style.symbol_field;
         }
+
         this.draw_text_normalized(p.value, p.effects);
+
         this.pop();
     }
 
