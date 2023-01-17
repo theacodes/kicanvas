e512811d76d478c099c52bb0f7f5dd5180ebc10a
Thea Flowers <thea@winterbloom.com>
2023-01-17 12:27:49 -0500

    Add docs for gl.js

--- src/gfx/gl.js
+++ src/gfx/gl.js
@@ -4,6 +4,18 @@
     Full text available at: https://opensource.org/licenses/MIT
 */
 
+/**
+ * Basic helpers for interacting with WebGL2
+ */
+
+/**
+ * Encapsulates a shader uniform, making it easier to set values.
+ *
+ * @example
+ * u_color = new Uniform(gl, "u_color", u_color_location);
+ * u_color.f4(1, 0, 1, 1);
+ *
+ */
 class Uniform {
     static _function_map = {
         f1: "uniform1f",
@@ -36,11 +48,32 @@ class Uniform {
     }
 }
 
+/**
+ * A shader program consisting of a vertex shader, fragment shader, and uniforms.
+ */
 export class ShaderProgram {
     static #shader_cache = {};
 
-    constructor(gl, vertex, fragment) {
+    /** Shader uniforms
+     * @type {Object.<string, Uniform>}
+     */
+    uniforms = {};
+
+    /** Shader attributes
+     * @type {Object.<string, GLint>}
+     */
+    attribs = {};
+
+    /**
+     * Create and compile a shader program
+     * @param {WebGL2RenderingContext} gl
+     * @param {string} name - used for caching and identifying the shader
+     * @param {string} vertex - vertex shader source code
+     * @param {string} fragment - fragment shader source code
+     */
+    constructor(gl, name, vertex, fragment) {
         this.gl = gl;
+        this.name = name;
 
         if (typeof vertex === "string") {
             vertex = this.constructor.compile(gl, gl.VERTEX_SHADER, vertex);
@@ -58,10 +91,19 @@ export class ShaderProgram {
 
         this.program = this.constructor.link(gl, vertex, fragment);
 
-        this.discover_uniforms();
-        this.discover_attribs();
+        this.#discover_uniforms();
+        this.#discover_attribs();
     }
 
+    /**
+     * Load vertex and fragment shader sources from URLs and compile them
+     * into a new ShaderProgram
+     * @param {WebGL2RenderingContext} gl
+     * @param {string} name - used for caching and identifying the shader.
+     * @param {URL|string} vert_url
+     * @param {URL|string} frag_url
+     * @returns
+     */
     static async load(gl, name, vert_url, frag_url) {
         if (!this.#shader_cache[name]) {
             const vert = await (
@@ -70,12 +112,22 @@ export class ShaderProgram {
             const frag = await (
                 await fetch(new URL(frag_url, import.meta.url))
             ).text();
-            this.#shader_cache[name] = new ShaderProgram(gl, vert, frag);
+            this.#shader_cache[name] = new ShaderProgram(gl, name, vert, frag);
         }
 
         return this.#shader_cache[name];
     }
 
+    /**
+     * Compiles a shader
+     *
+     * Typically not used directly, use load() instead.
+     *
+     * @param {WebGL2RenderingContext} gl
+     * @param {*} type - gl.FRAGMENT_SHADER or gl.VERTEX_SHADER
+     * @param {string} source
+     * @returns {WebGLShader}
+     */
     static compile(gl, type, source) {
         const shader = gl.createShader(type);
         gl.shaderSource(shader, source);
@@ -91,6 +143,16 @@ export class ShaderProgram {
         throw new Error(`Error compiling ${type} shader: ${info}`);
     }
 
+    /**
+     * Link a vertex and fragment shader into a shader program.
+     *
+     * Typically not used directly, use load() instead.
+     *
+     * @param {WebGL2RenderingContext} gl
+     * @param {WebGLShader} vertex
+     * @param {WebGLShader} fragment
+     * @returns {WebGLShader}
+     */
     static link(gl, vertex, fragment) {
         const program = gl.createProgram();
         gl.attachShader(program, vertex);
@@ -107,7 +169,7 @@ export class ShaderProgram {
         throw new Error(`Error linking shader program: ${info}`);
     }
 
-    discover_uniforms() {
+    #discover_uniforms() {
         this.uniforms = {};
         for (
             let u_n = 0;
@@ -129,7 +191,7 @@ export class ShaderProgram {
         }
     }
 
-    discover_attribs() {
+    #discover_attribs() {
         this.attribs = {};
         for (
             let a_n = 0;
@@ -150,36 +212,73 @@ export class ShaderProgram {
         }
     }
 
+    /** use this shader for drawing */
     bind() {
         this.gl.useProgram(this.program);
     }
 }
 
+/**
+ * Manages vertex array objects (VAOs) and associated buffers.
+ */
 export class VertexArray {
+    /**
+     * @type {Buffer[]}
+     */
+    buffers = [];
+
+    /**
+     * Create a VertexArray
+     * @param {WebGL2RenderingContext} gl
+     */
     constructor(gl) {
         this.gl = gl;
         this.vao = this.gl.createVertexArray();
         this.bind();
     }
 
-    dispose() {
+    /**
+     * Free WebGL resources
+     * @param {bool} include_buffers
+     */
+    dispose(include_buffers = true) {
         this.gl.deleteVertexArray(this.vao);
+        this.vao = null;
+
+        if (include_buffers) {
+            for (const buf in this.buffers) {
+                buf.dispose();
+            }
+        }
     }
 
     bind() {
         this.gl.bindVertexArray(this.vao);
     }
 
+    /**
+     * Create a new buffer bound to this vertex array
+     * @param {GLint} attrib - shader attribute location
+     * @param {GLint} size - number of components per vertex attribute
+     * @param {GLenum} type - data type for each component, if unspecified it's gl.FLOAT.
+     * @param {GLboolean} normalized - whether or not to normalize integer types when converting to float
+     * @param {GLsizei} stride - offset between consecutive attributes
+     * @param {GLintptr} offset - offset from the beginning of the array to the first attribute
+     * @param {GLenum} target - binding point, typically gl.ARRAY_BUFFER (the default if unspecified)
+     *      or gl.ELEMENT_ARRAY_BUFFER
+     * @returns {Buffer}
+     */
     buffer(
         attrib,
         size,
         type = null,
         normalized = false,
         stride = 0,
-        offset = 0
+        offset = 0,
+        target = null
     ) {
         type ??= this.gl.FLOAT;
-        const b = new Buffer(this.gl);
+        const b = new Buffer(this.gl, target);
 
         b.bind();
         this.gl.vertexAttribPointer(
@@ -192,35 +291,61 @@ export class VertexArray {
         );
         this.gl.enableVertexAttribArray(attrib);
 
+        this.buffers.push(b);
+
         return b;
     }
 }
 
+/**
+ * Manages a buffer of GPU data like vertices or colors
+ */
 export class Buffer {
-    constructor(gl) {
+    #buf;
+
+    /**
+     * Create a new buffer
+     * @param {WebGL2RenderingContext} gl
+     * @param {GLenum} target - binding point, typically gl.ARRAY_BUFFER (the default if unspecified)
+     *      or gl.ELEMENT_ARRAY_BUFFER
+     */
+    constructor(gl, target = null) {
         this.gl = gl;
-        this.buf = gl.createBuffer();
+        this.target = target ?? gl.ARRAY_BUFFER;
+        this.#buf = gl.createBuffer();
     }
 
     dispose() {
-        this.gl.deleteBuffer(this.buf);
+        this.gl.deleteBuffer(this.#buf);
+        this.#buf = null;
     }
 
-    bind(target = null) {
-        target ??= this.gl.ARRAY_BUFFER;
-        this.target = target;
-        this.gl.bindBuffer(target, this.buf);
+    /**
+     * Binds the buffer to the current context
+     */
+    bind() {
+        this.gl.bindBuffer(this.target, this.#buf);
     }
 
-    set(data, usage = null, target = null) {
-        this.bind(target);
+    /**
+     * Uploads data to the GPU buffer
+     *
+     * @param {TypedArray|DataView|ArrayBuffer|SharedArrayBuffer} data
+     * @param {GLenum} usage - intended usage pattern, typically gl.STATIC_DRAW (the default if unspecified) or gl.DYNAMIC_DRAW
+     */
+    set(data, usage = null) {
+        this.bind();
         usage ??= this.gl.STATIC_DRAW;
         this.gl.bufferData(this.target, data, usage);
     }
 
-    length(target = null) {
-        target ??= this.gl.ARRAY_BUFFER;
-        this.bind(target);
-        return this.gl.getBufferParameter(target, this.gl.BUFFER_SIZE);
+    /**
+     * Gets the length of the buffer as reported by WebGL.
+     * @param {*} target
+     * @returns
+     */
+    get length() {
+        this.bind();
+        return this.gl.getBufferParameter(this.target, this.gl.BUFFER_SIZE);
     }
 }
