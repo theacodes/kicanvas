f8f8d002094b74b83f9e3171aad4b9934fb7fa44
Thea Flowers <thea@winterbloom.com>
2023-01-22 21:43:00 -0500

    Add prettier config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,7 +6,11 @@ module.exports = {
         browser: true,
         es2021: true,
     },
-    extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+    extends: [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "prettier",
+    ],
     overrides: [],
     parserOptions: {
         ecmaVersion: "latest",
