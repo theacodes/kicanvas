47fc3e25f2630b7479d7c75e77c31372fc7795a4
Thea Flowers <thea@winterbloom.com>
2023-01-26 18:08:59 -0500

    Add framework/layers.ts and framework/viewer.ts

---
+++ src/framework/layers.ts
@@ -0,0 +1,170 @@
+/*
+    Copyright (c) 2023 Alethea Katherine Flowers.
+    Published under the standard MIT License.
+    Full text available at: https://opensource.org/licenses/MIT
+*/
+
+import { Color } from "../gfx/color.js";
+import { RenderLayer } from "../gfx/renderer.js";
+import { BBox } from "../math/bbox.js";
+import { Vec2 } from "../math/vec2.js";
+
+/**
+ * View layers
+ *
+ * KiCanvas's structure uses view layers to gather schematic or board items.
+ * These view layers are used render parts of each item in the correct order
+ * (back to front) as well as provide bounding box queries.
+ *
+ * For the board viewer, some layers correspond to physical board layers
+ * (like F.Cu, F.SilkS, & so on) but many are "virtual". The schematic viewer
+ * uses layers as well but largely to get the correct rendering order.
+ */
+
+type VisibilityType = boolean | (() => boolean);
+
+/**
+ * A view layer
+ */
+export class Layer {
+    #layer_set: LayerSet;
+    #visible: VisibilityType;
+    name: string;
+
+    /**
+     * The layer color can be used by Painters as a default or fallback color
+     * for items.
+     */
+    color: Color;
+
+    /**
+     * Board or schematic items on this layer.
+     */
+    items: any[];
+
+    /**
+     * This stores all of the graphics created by painters for items on this layer.
+     * */
+    graphics: RenderLayer;
+
+    /** A map of board items to bounding boxes
+     * A board item can have graphics on multiple layers, the bounding box provided
+     * here is only valid for this layer.
+     */
+    bboxes: Map<any, BBox> = new Map();
+
+    /**
+     * Create a new Layer.
+     * @param  ayer_set - the LayerSet that this Layer belongs to
+     * @param name - this layer's name
+     * @param visible - controls whether the layer is visible when rendering, may be a function returning a boolean.
+     */
+    constructor(
+        layer_set: LayerSet,
+        name: string,
+        visible: VisibilityType = true,
+        color: Color = Color.white
+    ) {
+        this.#layer_set = layer_set;
+        this.name = name;
+        this.color = color;
+        this.#visible = visible;
+        this.items = [];
+    }
+
+    get visible(): boolean {
+        if (this.#visible instanceof Function) {
+            return this.#visible();
+        } else {
+            return this.#visible;
+        }
+    }
+
+    set visible(v: VisibilityType) {
+        this.#visible = v;
+    }
+
+    /** The overall bounding box of all items on this layer */
+    get bbox() {
+        return BBox.combine(this.bboxes.values());
+    }
+
+    /** @yields a list of BBoxes that contain the given point */
+    *query_point(p: Vec2) {
+        for (const bb of this.bboxes.values()) {
+            if (bb.contains_point(p)) {
+                yield bb;
+            }
+        }
+    }
+}
+
+/**
+ * Represents the complete set of view layers.
+ */
+export class LayerSet {
+    #layer_list: Layer[] = [];
+    #layer_map: Map<string, Layer> = new Map();
+
+    /**
+     * Create a new LayerSet
+     */
+    constructor() {}
+
+    /**
+     * Adds layers to the set. Layers should be added front to back.
+     */
+    add(...layers: Layer[]) {
+        for (const layer of layers) {
+            this.#layer_list.push(layer);
+            this.#layer_map.set(layer.name, layer);
+        }
+    }
+
+    /**
+     * @yields layers in the order they were added (front to back)
+     */
+    *in_order() {
+        for (const layer of this.#layer_list) {
+            yield layer;
+        }
+    }
+
+    /**
+     * @yields layers in the order they should be drawn (back to front)
+     */
+    *in_display_order() {
+        for (let i = this.#layer_list.length - 1; i >= 0; i--) {
+            yield this.#layer_list[i];
+        }
+    }
+
+    /**
+     * Gets a Layer by name
+     */
+    by_name(name: string): Layer {
+        return this.#layer_map.get(name);
+    }
+
+    /**
+     * @yields a list of interactive layers
+     */
+    *interactive_layers() {
+        for (const layer of this.in_order()) {
+            if (layer.visible) {
+                yield layer;
+            }
+        }
+    }
+
+    /**
+     * @yields layers and bounding boxes that contain the given point.
+     */
+    *query_point(p: Vec2) {
+        for (const layer of this.interactive_layers()) {
+            for (const bbox of layer.query_point(p)) {
+                yield { layer, bbox };
+            }
+        }
+    }
+}
---
+++ src/framework/viewer.ts
@@ -0,0 +1,111 @@
+/*
+    Copyright (c) 2022 Alethea Katherine Flowers.
+    Published under the standard MIT License.
+    Full text available at: https://opensource.org/licenses/MIT
+*/
+
+import { Viewport } from "../gfx/viewport.js";
+import { Renderer } from "../gfx/renderer";
+import { Vec2 } from "../math/vec2.js";
+import { BBox } from "../math/bbox.js";
+import { Color } from "../gfx/color.js";
+import { LayerSet } from "./layers.js";
+
+export abstract class Viewer extends EventTarget {
+    canvas: HTMLCanvasElement;
+    renderer: Renderer;
+    viewport: Viewport;
+    layers: LayerSet;
+    #selected: BBox;
+
+    constructor(canvas: HTMLCanvasElement) {
+        super();
+        this.canvas = canvas;
+        this.renderer = this.create_renderer(canvas);
+    }
+
+    abstract create_renderer(canvas: HTMLCanvasElement): Renderer;
+
+    async setup() {
+        await this.renderer.setup();
+
+        this.viewport = new Viewport(this.renderer, () => {
+            this.draw_soon();
+        });
+
+        this.viewport.enable_pan_and_zoom(0.3, 200);
+
+        this.#setup_events();
+    }
+
+    #setup_events() {
+        this.canvas.addEventListener("click", (e) => {
+            const rect = this.canvas.getBoundingClientRect();
+            const mouse = this.viewport.screen_to_world(
+                new Vec2(e.clientX - rect.left, e.clientY - rect.top)
+            );
+
+            this.selected = null;
+
+            const items = this.layers.query_point(mouse);
+
+            this.dispatchEvent(
+                new CustomEvent("kicanvas:viewer:select", {
+                    detail: {
+                        mouse: mouse,
+                        items: items,
+                    },
+                })
+            );
+        });
+    }
+
+    abstract load(url: string | URL): Promise<void>;
+
+    protected abstract draw();
+
+    draw_soon() {
+        if (!this.viewport) {
+            return;
+        }
+
+        window.requestAnimationFrame(() => {
+            this.renderer.clear_canvas();
+            this.draw();
+        });
+    }
+
+    get selected() {
+        return this.#selected;
+    }
+
+    set selected(bb: BBox) {
+        this.#selected = bb?.copy();
+        this.paint_selected();
+    }
+
+    get selection_color() {
+        return Color.white;
+    }
+
+    paint_selected() {
+        const layer = this.layers.by_name(":Overlay");
+
+        layer.graphics?.clear();
+
+        if (this.#selected) {
+            const bb = this.#selected.copy().grow(this.#selected.w * 0.1);
+            this.renderer.start_layer(layer.name, 1);
+
+            this.renderer.line(bb.to_polyline(0.127, this.selection_color));
+
+            this.renderer.polygon(
+                bb.to_polygon(this.selection_color.with_alpha(0.4))
+            );
+
+            layer.graphics = this.renderer.end_layer();
+        }
+
+        this.draw_soon();
+    }
+}
