775dae8368a50916de6229af670d5f5aff67b1d1
Thea Flowers <thea@winterbloom.com>
2023-01-27 12:23:33 -0500

    Add implementation of render() for Viewer

--- src/framework/view-layers.ts
+++ src/framework/view-layers.ts
@@ -7,6 +7,7 @@
 import { Color } from "../gfx/color";
 import { RenderLayer } from "../gfx/renderer";
 import { BBox } from "../math/bbox";
+import { Matrix3 } from "../math/matrix3";
 import { Vec2 } from "../math/vec2";
 
 /**
@@ -118,6 +119,17 @@ export class ViewLayerSet {
         for (const layer of layers) {
             this.#layer_list.push(layer);
             this.#layer_map.set(layer.name, layer);
+        }
+    }
+
+    /**
+     * Renders all the layers in the set in display order (back to front)
+     */
+    render(camera: Matrix3) {
+        for (const layer of this.in_display_order()) {
+            if (layer.visible && layer.graphics) {
+                layer.graphics.render(camera);
+            }
         }
     }
 
