d44eae7c7617ad66c08d2a3df94793bc7ad5fc15
Thea Flowers <thea@winterbloom.com>
2023-01-30 13:51:29 -0500

    Use CustomElement for KicanvasApp

--- src/elements/kicanvas-app.ts
+++ src/elements/kicanvas-app.ts
@@ -10,17 +10,19 @@ import { KiCanvasSchematicElement } from "./kicanvas-schematic";
 import { KiCanvasBoardElement } from "./kicanvas-board";
 import { KiCanvasLayerControlsElement } from "./kicanvas-layer-controls";
 import { KiCanvasInfoBarElement } from "./kicanvas-info-bar";
-import "./kicanvas-dialog";
+import { CustomElement, html } from "../framework/elements";
+import styles from "./kicanvas-app.css";
 
-class KiCanvasAppElement extends HTMLElement {
-    #view_elm: KiCanvasSchematicElement | KiCanvasBoardElement;
+class KiCanvasAppElement extends CustomElement {
+    static styles = styles;
 
     constructor() {
         super();
     }
 
     async connectedCallback() {
-        this.#renderShadowDOM();
+        super.connectedCallback();
+
         new DropTarget(this, ["kicad_sch", "kicad_pcb"], (files) => {
             this.load(files[0]);
         });
@@ -30,127 +32,64 @@ class KiCanvasAppElement extends HTMLElement {
         this.setAttribute("loading", "");
 
         this.shadowRoot.querySelector("main")?.remove();
-        const main_elm = document.createElement("main");
 
         const extension = src.name.split(".").at(-1);
 
+        let view_elem: KiCanvasSchematicElement | KiCanvasBoardElement;
+        let content;
+
         switch (extension) {
             case "kicad_sch":
-                main_elm.innerHTML =
-                    "<kicanvas-schematic></kicanvas-schematic>";
-                this.shadowRoot.appendChild(main_elm);
-                this.#view_elm =
-                    this.shadowRoot.querySelector("kicanvas-schematic");
+                view_elem =
+                    html`<kicanvas-schematic></kicanvas-schematic>` as KiCanvasSchematicElement;
+                content = html`<main>${view_elem}</main>`;
                 break;
 
             case "kicad_pcb":
                 {
-                    main_elm.innerHTML = `
-                <div class="split-horizontal">
-                    <div class="split-vertical">
-                        <kicanvas-board></kicanvas-board>
-                    </div>
-                </div>`;
+                    view_elem =
+                        html`<kicanvas-board></kicanvas-board>` as KiCanvasBoardElement;
 
-                    this.shadowRoot.appendChild(main_elm);
-                    this.#view_elm =
-                        this.shadowRoot.querySelector("kicanvas-board");
+                    const layer_controls_elem =
+                        html`<kicanvas-layer-controls></kicanvas-layer-controls>` as KiCanvasLayerControlsElement;
+                    layer_controls_elem.target = view_elem;
 
-                    const layer_controls = document.createElement(
-                        "kicanvas-layer-controls"
-                    ) as KiCanvasLayerControlsElement;
-                    layer_controls.target = this
-                        .#view_elm as KiCanvasBoardElement;
-                    this.#view_elm.after(layer_controls);
+                    const info_bar_elem =
+                        html`<kicanvas-info-bar></kicanvas-info-bar>` as KiCanvasInfoBarElement;
+                    info_bar_elem.target = view_elem;
 
-                    const info_bar = document.createElement(
-                        "kicanvas-info-bar"
-                    ) as KiCanvasInfoBarElement;
-                    info_bar.target = this.#view_elm as KiCanvasBoardElement;
-                    this.#view_elm.parentElement.after(info_bar);
+                    content = html`<main>
+                        <div class="split-horizontal">
+                            <div class="split-vertical">
+                                ${view_elem} ${layer_controls_elem}
+                            </div>
+                            ${info_bar_elem}
+                        </div>
+                    </main>`;
                 }
                 break;
             default:
                 throw new Error(`Unable to display file ${src.name}`);
         }
 
-        await this.#view_elm.load(src);
+        this.shadowRoot.appendChild(content);
+
+        await view_elem.load(src);
 
         this.setAttribute("loaded", "");
         this.removeAttribute("loading");
     }
 
-    #renderShadowDOM() {
-        const template = document.createElement("template");
-        template.innerHTML = `
-            <style>
-                *,
-                *::before,
-                *::after {
-                    box-sizing: border-box;
-                }
+    async render() {
+        this.style.backgroundColor = theme.schematic.background.to_css();
+        this.style.color = theme.schematic.note.to_css();
 
-                * {
-                    margin: 0;
-                }
-
-                :host {
-                    box-sizing: border-box;
-                    margin: 0;
-                    display: flex;
-                    position: relative;
-                    width: 100%;
-                    height: 100%;
-                    background-color: ${theme.schematic.background.to_css()};
-                    color: ${theme.schematic.note.to_css()};
-                }
-
-                :host([loaded]) overlay, :host([loading]) overlay {
-                    display: none;
-                }
-
-                kicanvas-schematic, kicanvas-board {
-                    width: auto;
-                    height: 100%;
-                    flex: 1;
-                }
-
-                overlay {
-                    position: absolute;
-                    top: 0;
-                    left: 0;
-                    width: 100%;
-                    height: 100%;
-                    z-index: 1;
-                    display: flex;
-                    align-items: center;
-                    justify-content: center;
-                }
-
-                .split-horizontal {
-                    display: flex;
-                    flex-direction: column;
-                    height: 100%;
-                    max-height: 100%;
-                    overflow: hidden;
-                }
-
-                .split-vertical {
-                    display: flex;
-                    flex-direction: row;
-                    width: 100%;
-                    max-width: 100%;
-                    height: 100%;
-                    overflow: hidden;
-                }
-            </style>
-
-            <overlay>Drag & drop your kicad schematic or board file here.</overlay>
+        return html`
+            <overlay
+                >Drag & drop your kicad schematic or board file here.</overlay
+            >
             <kicanvas-dialog></kicanvas-dialog>
         `;
-
-        const root = this.attachShadow({ mode: "open" });
-        root.appendChild(template.content.cloneNode(true));
     }
 }
 
