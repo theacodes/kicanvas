d27227d9664fc4f5dc0c0f3eeb2945b989058cfc
Thea Flowers <thea@winterbloom.com>
2023-02-01 14:38:04 -0500

    Add newparser and test for it

---
+++ src/kicad/newparser.ts
@@ -0,0 +1,325 @@
+import { type List } from "./tokenizer.ts";
+import { Vec2 } from "../math/vec2.ts";
+
+enum Kind {
+    // the first token in the expr (kind ...)
+    start,
+    // (1 2 3) -> {name: value}
+    positional,
+    // ((name value)) -> {name: value}
+    pair,
+    // ((name value value value ...)) -> {name: [value, value, value]}
+    list,
+    // (locked [hide]) or mutually exclusively flags like (a | b | c)
+    atom,
+    // (name (kind 1) (kind 2) ...) -> {name: [item1, item2]}
+    item_list,
+    // generic expression (name 1 [2 3]) -> {name: [name, 1, [2, 3]]}
+    expr,
+}
+
+type Expr = number | string | List;
+type Obj = Record<string, any>;
+type Item = {
+    new (e: Expr, ...args: any[]): any;
+};
+type TypeProcessor = (obj: Obj, name: string, e: Expr) => any;
+type PropertyDefinition = {
+    name: string;
+    kind: Kind;
+    accepts?: string[];
+    fn: TypeProcessor;
+};
+
+/**
+ * Type processors.
+ * They transform an extracted value from the s-expr into the appropriate
+ * data type requested by the property definition.
+ */
+export const T = {
+    any(obj: Obj, name: string, e: Expr): any {
+        return e;
+    },
+    boolean(obj: Obj, name: string, e: Expr): boolean {
+        switch (e) {
+            case "false":
+            case "no":
+                return false;
+            case "true":
+            case "yes":
+                return true;
+            default:
+                return e ? true : false;
+        }
+    },
+    string(obj: Obj, name: string, e: Expr): string {
+        if (typeof e == "string") {
+            return e;
+        }
+    },
+    number(obj: Obj, name: string, e: Expr): number {
+        if (typeof e == "number") {
+            return e;
+        }
+    },
+    item(type: Item, ...args: any[]): TypeProcessor {
+        return (obj: Obj, name: string, e: Expr): any => {
+            return new type(e, ...args);
+        };
+    },
+    object(...defs: PropertyDefinition[]): TypeProcessor {
+        return (obj: Obj, name: string, e: Expr) => {
+            return parse_expr(e, P.start(name), ...defs);
+        };
+    },
+    vec2(obj: Obj, name: string, e: Expr): Vec2 {
+        return new Vec2(e[1], e[2]);
+    },
+};
+
+/**
+ * Property definitions
+ * These are used to describe the *shape* of the expected data along with the
+ * type processor needed to convert it to the right value.
+ */
+export const P = {
+    /**
+     * Checks that the first item in the list is "name". For example,
+     * (thing 1 2 3) would use start("thing").
+     */
+    start(name: string): PropertyDefinition {
+        return {
+            kind: Kind.start,
+            name: name,
+            fn: T.string,
+        };
+    },
+    /**
+     * Accepts a positional argument. For example,
+     * (1 2 3) with positional("first", T.number) would end up with {first: 1}.
+     */
+    positional(
+        name: string,
+        typefn: TypeProcessor = T.any,
+    ): PropertyDefinition {
+        return {
+            kind: Kind.positional,
+            name: name,
+            fn: typefn,
+        };
+    },
+    /**
+     * Accepts a pair. For example, ((a 1)) with pair(a) would end up with {a: 1}.
+     */
+    pair(name: string, typefn: TypeProcessor = T.any): PropertyDefinition {
+        return {
+            kind: Kind.pair,
+            name: name,
+            accepts: [name],
+            fn: (obj: Obj, name: string, e: Expr) => {
+                return typefn(obj, name, e[1]);
+            },
+        };
+    },
+    /**
+     * Accepts a list. For example ((a 1 2 3)) with list(a) would end up with {a: [1, 2, 3]}.
+     */
+    list(name: string, typefn: TypeProcessor = T.any): PropertyDefinition {
+        return {
+            kind: Kind.list,
+            name: name,
+            accepts: [name],
+            fn: (obj: Obj, name: string, e: Expr) => {
+                return (e as List[]).slice(1).map((n) => typefn(obj, name, n));
+            },
+        };
+    },
+    /**
+     * Accepts a collection. For example ((a 1) (a 2) (a 3)) with collection("items", "a")
+     * would end up with {items: [[a, 1], [a, 2], [a, 3]]}.
+     */
+    collection(
+        name: string,
+        accept: string,
+        typefn: TypeProcessor = T.any,
+    ): PropertyDefinition {
+        return {
+            kind: Kind.item_list,
+            name: name,
+            accepts: [accept],
+            fn: (obj: Obj, name: string, e: Expr) => {
+                const list = obj[name] ?? [];
+                list.push(typefn(obj, name, e));
+                return list;
+            },
+        };
+    },
+    /**
+     * Accepts a dictionary. For example ((thing a 1) (thing b 2) (thing c 3)) with
+     * dict("things", "thing") would end up with {things: {a: 1, b: 2, c: 3}}.
+     */
+    dict(
+        name: string,
+        accept: string,
+        typefn: TypeProcessor = T.any,
+    ): PropertyDefinition {
+        return {
+            kind: Kind.item_list,
+            name: name,
+            accepts: [accept],
+            fn: (obj: Obj, name: string, e: Expr) => {
+                const rec = obj[name] ?? {};
+                rec[e[1]] = typefn(obj, name, e[2]);
+                return rec;
+            },
+        };
+    },
+    /**
+     * Accepts an atom. For example (locked) and ((locked)) with atom("locked")
+     * would end up with {locked: true}. Atoms can also be mutually exclusive
+     * options, for example atom("align", ["left", "right"]) would process
+     * (left) as {align: "left"} and (right) as {align: "right"}.
+     */
+    atom(name: string, values: string[] = null): PropertyDefinition {
+        let typefn;
+
+        if (values) {
+            typefn = T.string;
+        } else {
+            typefn = T.boolean;
+            values = [name];
+        }
+
+        return {
+            kind: Kind.atom,
+            name: name,
+            accepts: values,
+            fn(obj: Obj, name: string, e: Expr) {
+                // Handle "(atom)" as "atom".
+                if (Array.isArray(e) && e.length == 1) {
+                    e = e[0];
+                }
+                return typefn(obj, name, e);
+            },
+        };
+    },
+    /**
+     * Accepts an expression. For example ((thing a 1 b)) with expr("thing")
+     * would end up with {thing: ["thing", a, 1, b]}.
+     */
+    expr(name: string, typefn: TypeProcessor = T.any): PropertyDefinition {
+        return {
+            kind: Kind.expr,
+            name: name,
+            accepts: [name],
+            fn: typefn,
+        };
+    },
+    /**
+     * Accepts an expression that describes a simple object with the given
+     * property definitions. For example ((thing (a 1) (b 2))) with
+     * object("thing", P.pair("a"), P.pair("b")) would end up with
+     * {thing: {a: 1, b: 2}}.
+     */
+    object(name: string, ...defs: PropertyDefinition[]): PropertyDefinition {
+        return P.expr(name, T.object(...defs));
+    },
+    /**
+     * Accepts an expression that describes an object that can be used to
+     * construct the given Item type. An Item is any class that takes
+     * a List as its first constructor parameter.
+     */
+    item(name: string, item_type: Item, ...args: any[]): PropertyDefinition {
+        return P.expr(name, T.item(item_type, ...args));
+    },
+    /**
+     * Accepts an expression that describes a 2d vector. For example,
+     * ((xy 1 2)) with vec2("xy") would end up with {xy: Vec2(1, 2)}.
+     */
+    vec2(name) {
+        return P.expr(name, T.vec2);
+    },
+};
+
+export function parse_expr(expr, ...defs) {
+    const defs_map = new Map();
+
+    let start_def;
+    let n = 0;
+
+    for (const def of defs) {
+        if (def.kind == Kind.start) {
+            start_def = def;
+        } else if (def.kind == Kind.positional) {
+            defs_map.set(n, def);
+            n++;
+        } else {
+            for (const a of def.accepts) {
+                defs_map.set(a, def);
+            }
+        }
+    }
+
+    if (start_def) {
+        if (typeof start_def.name == "string") {
+            start_def.name = [start_def.name];
+        }
+
+        const first = expr.at(0);
+
+        if (!start_def.name.includes(first)) {
+            console.log(
+                `Expression must start with ${start_def.name}, found:`,
+                first,
+            );
+            return null;
+        }
+
+        expr = expr.slice(1);
+    }
+
+    const out = {};
+
+    n = 0;
+    for (const element of expr) {
+        let def;
+
+        // bare string value can be an atom
+        if (typeof element == "string") {
+            def = defs_map.get(element);
+        }
+
+        // If not an atom, a bare string or number can be a positional
+        if (
+            !def &&
+            (typeof element == "string" || typeof element == "number")
+        ) {
+            def = defs_map.get(n);
+
+            if (!def) {
+                console.trace(
+                    `no def for bare element ${element} at position ${n} in expression`,
+                );
+                continue;
+            }
+
+            n++;
+        }
+
+        // list of elements
+        if (!def && Array.isArray(element)) {
+            def = defs_map.get(element[0]);
+        }
+
+        if (!def) {
+            console.log("No def found for element", element);
+            continue;
+        }
+
+        const value = def.fn(out, def.name, element);
+
+        out[def.name] = value;
+    }
+
+    return out;
+}
---
+++ test/parser.test.ts
@@ -0,0 +1,272 @@
+/*
+    Copyright (c) 2022 Alethea Katherine Flowers.
+    Published under the standard MIT License.
+    Full text available at: https://opensource.org/licenses/MIT
+*/
+
+import { assert } from "@esm-bundle/chai";
+
+import { listify } from "../src/kicad/tokenizer";
+import { T, P, parse_expr } from "../src/kicad/newparser";
+import { Vec2 } from "../src/math/vec2";
+
+suite("Parser", function () {
+    test("start", function () {
+        let res;
+
+        res = parse_expr(listify("thing 1 2"), P.start("thing"));
+        assert.deepEqual(res, {});
+
+        res = parse_expr(listify("notthing 1 2"), P.start("thing"));
+        assert.deepEqual(res, null);
+    });
+
+    test("positional", function () {
+        let res;
+
+        res = parse_expr(
+            listify("1 2"),
+            P.positional("first"),
+            P.positional("second"),
+        );
+        assert.deepEqual(res, { first: 1, second: 2 });
+
+        res = parse_expr(
+            listify("1 a"),
+            P.positional("first"),
+            P.positional("second", T.number),
+        );
+        assert.deepEqual(res, { first: 1, second: undefined });
+
+        res = parse_expr(
+            listify("1 2"),
+            P.positional("first", T.string),
+            P.positional("second", T.number),
+        );
+        assert.deepEqual(res, { first: undefined, second: 2 });
+    });
+
+    test("pair", function () {
+        const res = parse_expr(
+            listify("(a 1) (c 3) (b 2) d"),
+            P.pair("a"),
+            P.pair("b"),
+            P.pair("c"),
+            P.pair("d"),
+        );
+        assert.deepEqual(res, { a: 1, b: 2, c: 3, d: undefined });
+    });
+
+    test("list", function () {
+        const res = parse_expr(
+            listify("(a 1) (b 1 2 3) (c c1 c2 3) (d)"),
+            P.list("a"),
+            P.list("b"),
+            P.list("c"),
+            P.list("d"),
+        );
+        assert.deepEqual(res, {
+            a: [1],
+            b: [1, 2, 3],
+            c: ["c1", "c2", 3],
+            d: [],
+        });
+    });
+
+    test("collection", function () {
+        const res = parse_expr(
+            listify("(foo a 1) (bar b 2) (baz c 3) (foo d 4)"),
+            P.collection("foos_and_bars", "foo"),
+            P.collection("foos_and_bars", "bar"),
+            P.collection("bazs", "baz"),
+        );
+        assert.deepEqual(res, {
+            foos_and_bars: [
+                ["foo", "a", 1],
+                ["bar", "b", 2],
+                ["foo", "d", 4],
+            ],
+            bazs: [["baz", "c", 3]],
+        });
+    });
+
+    test("dict", function () {
+        const res = parse_expr(
+            listify("(def a 1) (def b 2) (def c (3 4))"),
+            P.dict("defs", "def"),
+        );
+        assert.deepEqual(res, {
+            defs: {
+                a: 1,
+                b: 2,
+                c: [3, 4],
+            },
+        });
+    });
+
+    test("atom", function () {
+        const defs = [
+            P.atom("locked"),
+            P.atom("enabled", ["yes", "no"]),
+            P.atom("horizontal", ["left", "right"]),
+            P.atom("color", ["blue", "green", "red"]),
+            P.atom("vertical", ["top", "bottom"]),
+        ];
+
+        const cases = [
+            {
+                src: "locked yes left (blue) top",
+                expected: {
+                    locked: true,
+                    enabled: "yes",
+                    horizontal: "left",
+                    color: "blue",
+                    vertical: "top",
+                },
+            },
+            {
+                src: "no right top red",
+                expected: {
+                    enabled: "no",
+                    horizontal: "right",
+                    color: "red",
+                    vertical: "top",
+                },
+            },
+            {
+                src: "(yes) (top) (bottom)",
+                expected: {
+                    enabled: "yes",
+                    vertical: "bottom",
+                },
+            },
+        ];
+
+        for (const { src, expected } of cases) {
+            const res = parse_expr(listify(src), ...defs);
+            assert.deepEqual(res, expected);
+        }
+    });
+
+    test("expr", function () {
+        let res;
+
+        res = parse_expr(listify("(expr 1 2 3)"), P.expr("expr"));
+        assert.deepEqual(res, {
+            expr: ["expr", 1, 2, 3],
+        });
+
+        res = parse_expr(listify("(expr)"), P.expr("expr"));
+        assert.deepEqual(res, {
+            expr: ["expr"],
+        });
+
+        res = parse_expr(listify("expr"), P.expr("expr"));
+        assert.deepEqual(res, {
+            expr: "expr",
+        });
+    });
+
+    test("object", function () {
+        const res = parse_expr(
+            listify("(thing (a 1) (b 2) yes)"),
+            P.object(
+                "thing",
+                P.pair("a"),
+                P.pair("b"),
+                P.atom("enabled", ["yes", "no"]),
+            ),
+        );
+        assert.deepEqual(res, {
+            thing: { a: 1, b: 2, enabled: "yes" },
+        });
+    });
+
+    test("item", function () {
+        const res = parse_expr(
+            listify("(item (a 1) (b 2) yes)"),
+            P.item("item", TestItem, "arg1", "arg2"),
+        );
+
+        assert.deepEqual(res, {
+            item: new TestItem(
+                ["item", ["a", 1], ["b", 2], "yes"],
+                "arg1",
+                "arg2",
+            ),
+        });
+    });
+
+    test("vec2", function () {
+        const res = parse_expr(
+            listify("(at 1 2) (xy 3 4)"),
+            P.vec2("at"),
+            P.vec2("xy"),
+        );
+
+        assert.deepEqual(res, {
+            at: new Vec2(1, 2),
+            xy: new Vec2(3, 4),
+        });
+    });
+
+    test("complex", function () {
+        const src = `
+            (example
+                abcdef
+                4
+                (layer layer1)
+                (layers layer1 layer2)
+                type_a
+                (hide)
+                locked
+                (width 0.5)
+                (start 1 2)
+                (gr_rect 1 2 3)
+                (gr_poly a b c)
+                (pts (xy 1 1) (xy 2 2)))`;
+
+        const expr = listify(src);
+
+        const res = parse_expr(
+            expr[0],
+            P.start("example"),
+            P.positional("text", T.string),
+            P.positional("size", T.number),
+            P.pair("layer", T.string),
+            P.list("layers", T.string),
+            P.list("start", T.number),
+            P.pair("width", T.number),
+            P.collection("graphics", "gr_rect", T.item(TestItem)),
+            P.collection("graphics", "gr_poly", T.item(TestItem)),
+            P.atom("type", ["type_a", "type_b", "type_c"]),
+            P.atom("hide"),
+            P.atom("locked"),
+            P.list("pts", T.item(TestItem)),
+        );
+
+        assert.deepEqual(res, {
+            text: "abcdef",
+            size: 4,
+            layer: "layer1",
+            layers: ["layer1", "layer2"],
+            start: [1, 2],
+            width: 0.5,
+            graphics: [
+                new TestItem(["gr_rect", 1, 2, 3]),
+                new TestItem(["gr_poly", "a", "b", "c"]),
+            ],
+            type: "type_a",
+            hide: true,
+            locked: true,
+            pts: [new TestItem(["xy", 1, 1]), new TestItem(["xy", 2, 2])],
+        });
+    });
+});
+
+class TestItem {
+    args: any[];
+    constructor(public expr, ...args) {
+        this.args = args;
+    }
+}
