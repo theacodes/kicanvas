6b9aa4cfde755721f87e48e842596a7f995a5a57
Thea Flowers <thea@winterbloom.com>
2023-02-01 18:21:34 -0500

    Include .kicad_sch and .kicad_pcb files in web test runner

--- web-test-runner.config.mjs
+++ web-test-runner.config.mjs
@@ -4,7 +4,18 @@ import { esbuildPlugin } from "@web/dev-server-esbuild";
 // https://modern-web.dev/docs/test-runner/cli-and-configuration/
 
 export default {
-    plugins: [esbuildPlugin({ ts: true })],
+    plugins: [
+        esbuildPlugin({
+            ts: true,
+            loaders: {
+                ".js": "ts",
+                ".glsl": "text",
+                ".css": "text",
+                ".kicad_pcb": "text",
+                ".kicad_sch": "text",
+            },
+        }),
+    ],
     reporters: [
         defaultReporter({ reportTestResults: true, reportTestProgress: true }),
         summaryReporter(),
