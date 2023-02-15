7f0d29173593f5c9d4b8a1ce4d9d35f330fec051
Thea Flowers <thea@winterbloom.com>
2023-02-14 19:22:21 -0500

    Specialize apply_at for SchText and add set_spin_style_from_angle

--- src/text/sch_text.ts
+++ src/text/sch_text.ts
@@ -4,6 +4,8 @@
     Full text available at: https://opensource.org/licenses/MIT
 */
 
+import { At } from "../kicad/common";
+import { Angle } from "../math/angle";
 import { EDAText } from "./eda_text";
 
 /**
@@ -13,6 +15,39 @@ import { EDAText } from "./eda_text";
 export class SchText extends EDAText {
     constructor(text: string) {
         super(text);
+    }
+
+    override apply_at(at: At): void {
+        super.apply_at(at);
+        this.set_spin_style_from_angle(this.text_angle);
+    }
+
+    set_spin_style_from_angle(a: Angle) {
+        switch (a.degrees) {
+            default:
+            case 0:
+                // right
+                this.text_angle.degrees = 0;
+                this.h_align = "left";
+                break;
+            case 90:
+                // up
+                this.text_angle.degrees = 90;
+                this.h_align = "left";
+                break;
+            case 180:
+                //left
+                this.text_angle.degrees = 0;
+                this.h_align = "right";
+                break;
+            case 270:
+                // down
+                this.text_angle.degrees = 90;
+                this.h_align = "right";
+                break;
+        }
+
+        this.v_align = "bottom";
     }
 
     override get shown_text() {
