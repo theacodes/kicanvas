1c92d6cd1dcbd435e518d3742d3dcca81e6ed9ce
Thea Flowers <thea@winterbloom.com>
2023-02-28 13:07:08 -0500

    Add predefined paper sizes in mm

--- src/kicad/common.ts
+++ src/kicad/common.ts
@@ -67,25 +67,28 @@ export class At {
     }
 }
 
-export type PaperSize =
-    | "User"
-    | "A0"
-    | "A1"
-    | "A2"
-    | "A3"
-    | "A4"
-    | "A5"
-    | "A"
-    | "B"
-    | "C"
-    | "D"
-    | "E"
-    | "USLetter"
-    | "USLegal"
-    | "USLedger";
+export const PaperSize = {
+    User: [431.8, 279.4] as const,
+    A0: [1189, 841] as const,
+    A1: [841, 594] as const,
+    A2: [594, 420] as const,
+    A3: [420, 297] as const,
+    A4: [297, 210] as const,
+    A5: [210, 148] as const,
+    A: [279.4, 215.9] as const,
+    B: [431.8, 279.4] as const,
+    C: [558.8, 431.8] as const,
+    D: [863.6, 558.8] as const,
+    E: [1117.6, 863.6] as const,
+    USLetter: [279.4, 215.9] as const,
+    USLegal: [355.6, 215.9] as const,
+    USLedger: [431.8, 279.4] as const,
+};
+
+export type PaperSizeName = keyof typeof PaperSize;
 
 export class Paper {
-    size: string;
+    size: PaperSizeName;
     width?: number;
     height?: number;
     portrait = false;
@@ -96,28 +99,22 @@ export class Paper {
             parse_expr(
                 expr,
                 P.start("paper"),
-                P.atom("size", [
-                    "User",
-                    "A0",
-                    "A1",
-                    "A2",
-                    "A3",
-                    "A4",
-                    "A5",
-                    "A",
-                    "B",
-                    "C",
-                    "D",
-                    "E",
-                    "USLetter",
-                    "USLegal",
-                    "USLedger",
-                ]),
+                P.atom("size", Object.keys(PaperSize)),
                 P.positional("width", T.number),
                 P.positional("height", T.number),
                 P.atom("portrait"),
             ),
         );
+
+        const paper_size = PaperSize[this.size];
+
+        if (!this.width && paper_size) {
+            this.width = paper_size[0];
+        }
+
+        if (!this.height && paper_size) {
+            this.height = paper_size[1];
+        }
     }
 }
 
