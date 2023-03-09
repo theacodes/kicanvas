d8f703438b150edfc7a14e49af850f7d221dadfb
Thea Flowers <thea@winterbloom.com>
2023-03-09 12:31:42 -0500

    Add search to net panel

--- src/elements/kc-board/kc-board-nets-panel.ts
+++ src/elements/kc-board/kc-board-nets-panel.ts
@@ -6,7 +6,12 @@
 
 import { BoardViewer } from "../../board/viewer";
 import { WithContext } from "../../dom/context";
-import { html, CustomElement } from "../../dom/custom-elements";
+import { CustomElement, html } from "../../dom/custom-elements";
+import { KCUIFilteredListElement } from "../kc-ui/kc-ui-filtered-list";
+import { KCUITextFilterInputElement } from "../kc-ui/kc-ui-text-filter-input";
+
+import "../kc-ui/kc-ui-filtered-list";
+import "../kc-ui/kc-ui-text-filter-input";
 
 export class KCBoardNetsPanelElement extends WithContext(CustomElement) {
     static override useShadowRoot = false;
@@ -36,6 +41,20 @@ export class KCBoardNetsPanelElement extends WithContext(CustomElement) {
             this.viewer.highlight_net(number);
             this.mark_selected_item(number);
         });
+
+        // Wire up search to filter the list
+        this.search_input_elm.addEventListener("input", (e) => {
+            this.item_filter_elem.filter_text =
+                this.search_input_elm.value ?? null;
+        });
+    }
+
+    private get search_input_elm(): KCUITextFilterInputElement {
+        return this.renderRoot.querySelector("kc-ui-text-filter-input")!;
+    }
+
+    private get item_filter_elem(): KCUIFilteredListElement {
+        return this.renderRoot.querySelector("kc-ui-filtered-list")!;
     }
 
     private get items() {
@@ -58,9 +77,12 @@ export class KCBoardNetsPanelElement extends WithContext(CustomElement) {
 
         for (const net of board.nets) {
             nets.push(
-                html`<li data-number="${net.number}" aria-role="button">
-                    <span class="very-narrow">${net.number}</span
-                    ><span>${net.name}</span>
+                html` <li
+                    data-number="${net.number}"
+                    aria-role="button"
+                    data-match-text="${net.number} ${net.name}">
+                    <span class="very-narrow"> ${net.number} </span>
+                    <span>${net.name}</span>
                 </li>`,
             );
         }
@@ -71,9 +93,12 @@ export class KCBoardNetsPanelElement extends WithContext(CustomElement) {
                     <kc-ui-panel-header-text>Nets</kc-ui-panel-header-text>
                 </kc-ui-panel-header>
                 <kc-ui-panel-body class="no-padding">
-                    <ul class="item-list outline">
-                        ${nets}
-                    </ul>
+                    <kc-ui-text-filter-input></kc-ui-text-filter-input>
+                    <kc-ui-filtered-list>
+                        <ul class="item-list outline">
+                            ${nets}
+                        </ul>
+                    </kc-ui-filtered-list>
                 </kc-ui-panel-body>
             </kc-ui-panel>
         `;
