7b3d909e0f75de4f6c729bd6e0ec70b73b75292f
Thea Flowers <thea@winterbloom.com>
2023-03-15 15:12:32 -0400

    Add <kc-ui-toggle-menu>

---
+++ src/kc-ui/kc-ui-toggle-menu.ts
@@ -0,0 +1,49 @@
+/*
+    Copyright (c) 2023 Alethea Katherine Flowers.
+    Published under the standard MIT License.
+    Full text available at: https://opensource.org/licenses/MIT
+*/
+
+import { html, CustomElement } from "../base/elements/custom-element";
+import styles from "./kc-ui-toggle-menu.css";
+
+import "./kc-ui-dropdown";
+import { KCUIDropdownElement } from "./kc-ui-dropdown";
+import { delegate } from "../base/events";
+
+/**
+ * A toggle menu combines a button and a dropdown into a single element.
+ *
+ * This element holds a button and a kc-ui-dropdown, the button is used to
+ * toggle the dropdown.
+ */
+export class KCUIToggleMenuElement extends CustomElement {
+    static override styles = styles;
+
+    get dropdown() {
+        return this.queryAssignedElements<KCUIDropdownElement>(
+            "dropdown",
+            "kc-ui-dropdown",
+        )[0]!;
+    }
+
+    override initialContentCallback() {
+        delegate(this.renderRoot, `button[slot="button"]`, "click", (e) => {
+            this.dropdown.toggle();
+        });
+
+        this.addEventListener("kc-ui-dropdown:open", () => {
+            this.setBooleanAttribute("open", true);
+        });
+
+        this.addEventListener("kc-ui-dropdown:close", () => {
+            this.setBooleanAttribute("open", false);
+        });
+    }
+
+    override render() {
+        return html`<slot name="button"></slot> <slot name="dropdown"></slot>`;
+    }
+}
+
+window.customElements.define("kc-ui-toggle-menu", KCUIToggleMenuElement);
