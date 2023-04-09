5c98ea7880cd78a393eafb64319157ac9e982b51
Thea Flowers <thea@winterbloom.com>
2023-04-09 19:29:15 -0400

    Allow initiate_download to work on both files and URLs

--- src/base/dom/download.ts
+++ src/base/dom/download.ts
@@ -4,18 +4,34 @@
     Full text available at: https://opensource.org/licenses/MIT
 */
 
+import { basename } from "../paths";
+
 /*
     Basic helper to initiate a download of a given File using the browser.
     Useful for generating files client side for the user to download.
 */
 
-export function initiate_download(file: File) {
-    const url = URL.createObjectURL(file);
+export function initiate_download(file_or_url: File | URL) {
+    let url;
+    let name;
+
+    if (file_or_url instanceof File) {
+        url = URL.createObjectURL(file_or_url);
+        name = file_or_url.name;
+    } else {
+        url = file_or_url.href;
+        name = basename(url);
+    }
+
     const anchor = document.createElement("a");
 
     anchor.href = url;
-    anchor.download = file.name;
+    anchor.download = name;
+    anchor.target = "_blank";
+    console.log(anchor);
     anchor.click();
 
-    URL.revokeObjectURL(url);
+    if (file_or_url instanceof File) {
+        URL.revokeObjectURL(url);
+    }
 }
