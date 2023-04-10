8260fbb326f9cc2e1857f803016bb92ed47b9c20
Thea Flowers <thea@winterbloom.com>
2023-04-10 00:40:43 -0400

    Add paths.extension

--- src/base/paths.ts
+++ src/base/paths.ts
@@ -7,3 +7,7 @@
 export function basename(path: string | URL) {
     return new URL(path).pathname.split("/").at(-1)!;
 }
+
+export function extension(path: string) {
+    return path.split(".").at(-1) ?? "";
+}
