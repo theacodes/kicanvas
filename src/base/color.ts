8bf481b6717862496f8440a5d90f6bc5c2ccd1ca
Thea Flowers <thea@winterbloom.com>
2023-07-25 23:00:01 -0400

    Add Color.desaturate, Color.mix, rgb_to_hsl, and hsl_to_rgb

--- src/base/color.ts
+++ src/base/color.ts
@@ -117,4 +117,84 @@ export class Color {
         c.a = a;
         return c;
     }
+
+    desaturate() {
+        // From KiCAD's COLOR4D::Desaturate
+        if (this.r == this.g && this.r == this.b) {
+            return this;
+        }
+
+        const [h, _, l] = rgb_to_hsl(this.r, this.g, this.b);
+
+        return new Color(...hsl_to_rgb(h, 0.0, l));
+    }
+
+    mix(other: Color, amount: number) {
+        return new Color(
+            other.r * (1 - amount) + this.r * amount,
+            other.g * (1 - amount) + this.g * amount,
+            other.b * (1 - amount) + this.b * amount,
+            this.a,
+        );
+    }
+}
+
+/**
+ * Convert normalized RGB to HSL
+ */
+export function rgb_to_hsl(
+    r: number,
+    g: number,
+    b: number,
+): [number, number, number] {
+    const max = Math.max(...[r, g, b]);
+    const min = Math.min(...[r, g, b]);
+    const l = (min + max) / 2;
+    const d = max - min;
+    let [h, s] = [NaN, 0];
+
+    if (d !== 0) {
+        s = l === 0 || l === 1 ? 0 : (max - l) / Math.min(l, 1 - l);
+
+        switch (max) {
+            case r:
+                h = (g - b) / d + (g < b ? 6 : 0);
+                break;
+            case g:
+                h = (b - r) / d + 2;
+                break;
+            case b:
+                h = (r - g) / d + 4;
+        }
+
+        h = h * 60;
+    }
+
+    return [h, s * 100, l * 100];
+}
+
+/**
+ * Convert normalized HSL to RGB
+ */
+export function hsl_to_rgb(
+    h: number,
+    s: number,
+    l: number,
+): [number, number, number] {
+    h = h % 360;
+
+    if (h < 0) {
+        h += 360;
+    }
+
+    s /= 100;
+    l /= 100;
+
+    function f(n: number) {
+        const k = (n + h / 30) % 12;
+        const a = s * Math.min(l, 1 - l);
+        return l - a * Math.max(-1, Math.min(k - 3, 9 - k, 1));
+    }
+
+    return [f(0), f(8), f(4)];
 }
