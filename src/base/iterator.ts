61eda60a146dbf665f0c39446ac0c90502813056
Thea Flowers <thea@winterbloom.com>
2023-09-19 21:53:28 -0400

    Add iterator:map

--- src/base/iterator.ts
+++ src/base/iterator.ts
@@ -7,3 +7,16 @@
 export function first<T>(iterable: Iterable<T>): T | undefined {
     return iterable[Symbol.iterator]().next().value;
 }
+
+export type MapCallbackFn<T, U> = (value: T, index: number) => U;
+
+export function* map<T, U>(
+    iterable: Iterable<T>,
+    callback: MapCallbackFn<T, U>,
+): Generator<U, any, undefined> {
+    let n = 0;
+    for (const i of iterable) {
+        yield callback(i, n);
+        n++;
+    }
+}
