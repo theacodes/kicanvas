bc1b48379e17f41d2da6640d567d8ef50d524702
Thea Flowers <thea@winterbloom.com>
2023-09-20 21:53:43 -0400

    Add KCSchematicAppElement.sidebarCollapsed

--- src/kicanvas/elements/kc-schematic/app.ts
+++ src/kicanvas/elements/kc-schematic/app.ts
@@ -4,7 +4,7 @@
     Full text available at: https://opensource.org/licenses/MIT
 */
 
-import { html } from "../../../base/web-components";
+import { attribute, html } from "../../../base/web-components";
 import { KCUIActivitySideBarElement, KCUIElement } from "../../../kc-ui";
 import type { KicadSch } from "../../../kicad";
 import { SchematicSheet } from "../../../kicad/schematic";
@@ -41,7 +41,11 @@ export class KCSchematicAppElement extends KCUIElement {
         return this.viewer_elm.viewer;
     }
 
+    @attribute({ type: Boolean })
+    sidebar_collapsed: boolean;
+
     override initialContentCallback() {
+        console.log("collapsed?", this.sidebar_collapsed);
         this.addDisposable(
             this.viewer.addEventListener(KiCanvasSelectEvent.type, (e) => {
                 const item = e.detail.item;
@@ -68,6 +72,22 @@ export class KCSchematicAppElement extends KCUIElement {
         );
     }
 
+    attributeChangedCallback(
+        name: string,
+        old: string | null,
+        value: string | null,
+    ) {
+        switch (name) {
+            case "sidebarCollapsed":
+                if (this.activity_bar_elm) {
+                    this.activity_bar_elm.collapsed = !!value;
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
     async load(src: KicadSch | ProjectPage) {
         await this.viewer_elm.load(src);
     }
@@ -76,7 +96,8 @@ export class KCSchematicAppElement extends KCUIElement {
         this.viewer_elm =
             html`<kc-schematic-viewer></kc-schematic-viewer>` as KCSchematicViewerElement;
 
-        this.activity_bar_elm = html`<kc-ui-activity-side-bar>
+        this.activity_bar_elm = html`<kc-ui-activity-side-bar
+            collapsed="${this.sidebar_collapsed}">
             <kc-ui-activity slot="activities" name="Symbols" icon="interests">
                 <kc-schematic-symbols-panel></kc-schematic-symbols-panel>
             </kc-ui-activity>
@@ -102,7 +123,7 @@ export class KCSchematicAppElement extends KCUIElement {
             </kc-ui-activity>
         </kc-ui-activity-side-bar>` as KCUIActivitySideBarElement;
 
-        return html` <kc-ui-split-view vertical>
+        return html`<kc-ui-split-view vertical>
             <kc-ui-view class="grow">
                 ${this.viewer_elm}
                 <kc-viewer-bottom-toolbar></kc-viewer-bottom-toolbar>
